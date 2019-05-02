0ad507ccc381c7e2a1a1910143deb025a101c337
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-05-02 23:08:54 +0200

    Add files via upload

---
+++ HashTable/HashTable/main.cpp
@@ -0,0 +1,27 @@
+
+#include "hash.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	hashin MyHash;
+
+	string name, drink = "empty";
+
+	for(int i = 0 ; i < 10; i++) {
+		cout << "\nGive your name, and drink " << endl;
+		cin >> name;
+		cin >> drink;
+		MyHash.AddItem(name, drink);
+	}
+
+
+	MyHash.PrintTable();
+
+	system("pause");
+	
+	return 0;
+}
